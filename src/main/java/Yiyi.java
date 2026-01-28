c14c8b3b358b6c0970050f01e9e5a848069e10d4
Yian123330 <3034141790@qq.com>
2026-01-28 19:13:08 +0800

    Level-2: Add task storage and display

--- src/main/java/Yiyi.java
+++ src/main/java/Yiyi.java
@@ -2,9 +2,11 @@ import java.util.Scanner;
 
 public class Yiyi {
     private static final String LINE = "____________________________________________________________";
+    private static String[] tasks = new String[100];
+    private static int counts = 0;
     public static void main(String[] args) {
         greet();
-        echo();
+        inputTask();
         bye();
     }
 
@@ -21,18 +23,48 @@ public class Yiyi {
         System.out.println(LINE);
     }
 
-    private static void echo(){
+    private static void inputTask(){
         String line;
         Scanner in = new Scanner(System.in);
 
         while(true) {
-            line = in.nextLine();
+            line = in.nextLine().trim();
             if (line.equalsIgnoreCase("bye")) {
                 break;
+            }else if(line.equalsIgnoreCase("list")){
+                showList();
+            }else{
+                addTask(line);
             }
-            System.out.println(LINE);
-            System.out.println(line);
-            System.out.println(LINE);
         }
+    }
+
+    private static void addTask(String Description){
+        if(counts >= 100){
+            System.out.println(LINE);
+            System.out.println("Sorry, I can only store up to 100 tasks!");
+            System.out.println(LINE);
+            return;
+        }
+
+        tasks[counts] = Description;
+        counts++;
+        System.out.println(LINE);
+        System.out.println("added: " + Description);
+        System.out.println(LINE);
+    }
+
+    private static void showList(){
+        System.out.println(LINE);
+
+        if(counts == 0){
+            System.out.println("Your task list is empty!");
+        }else{
+            for(int i = 0; i < counts; i++){
+                System.out.println((i + 1) + ". " + tasks[i]);
+            }
+        }
+        System.out.println(LINE);
+
     }
 }
