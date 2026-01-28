9aa7219f6237a8c751fabdb0b62197bda6a55221
Yian123330 <3034141790@qq.com>
2026-01-28 18:22:31 +0800

    Level-1: echo

--- src/main/java/Yiyi.java
+++ src/main/java/Yiyi.java
@@ -1,7 +1,38 @@
+import java.util.Scanner;
+
 public class Yiyi {
+    private static final String LINE = "____________________________________________________________";
     public static void main(String[] args) {
+        greet();
+        echo();
+        bye();
+    }
+
+    private static void greet(){
+        System.out.println(LINE);
         System.out.println("Hello! I'm Yiyi");
         System.out.println("What can I do for you?");
+        System.out.println(LINE);
+    }
+
+    private static void bye(){
+        System.out.println(LINE);
         System.out.println("Bye. Hope to see you again!");
+        System.out.println(LINE);
+    }
+
+    private static void echo(){
+        String line;
+        Scanner in = new Scanner(System.in);
+
+        while(true) {
+            line = in.nextLine();
+            if (line.equalsIgnoreCase("bye")) {
+                break;
+            }
+            System.out.println(LINE);
+            System.out.println(line);
+            System.out.println(LINE);
+        }
     }
 }
