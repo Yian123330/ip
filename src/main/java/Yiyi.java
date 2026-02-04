cd16b8b3434e7a566be9e30badac69d5ffb2270e
Yian123330 <3034141790@qq.com>
2026-02-04 13:40:03 +0800

    Level-4: Add Todo, Deadline, Event classes with inheritance

--- src/main/java/Yiyi.java
+++ src/main/java/Yiyi.java
@@ -2,11 +2,12 @@ import java.util.Scanner;
 
 public class Yiyi {
     private static final String LINE = "____________________________________________________________";
-    private static Task[] tasks = new Task[100];
-    private static int counts = 0;
+    private static final int MAX_TASKS = 100;
+    private static Task[] tasks = new Task[MAX_TASKS];
+    private static int TaskCounts = 0;
     public static void main(String[] args) {
         greet();
-        inputTask();
+        handleUserInput();
         bye();
     }
 
@@ -23,7 +24,7 @@ public class Yiyi {
         System.out.println(LINE);
     }
 
-    private static void inputTask(){
+    private static void handleUserInput(){
         Scanner in = new Scanner(System.in);
 
         while(true) {
@@ -31,40 +32,134 @@ public class Yiyi {
             if (line.equalsIgnoreCase("bye")) {
                 break;
             }else if(line.equalsIgnoreCase("list")){
-                showList();
+                showTaskList();
             }else if(line.startsWith("mark ")){
                 markTask(line);
             }else if(line.startsWith("unmark ")){
                 unmarkTask(line);
+            }else if(line.startsWith("todo ")){
+                addTodo(line);
+            }else if(line.startsWith("deadline ")){
+                addDeadline(line);
+            }else if(line.startsWith("event ")){
+                addEvent(line);
             }else{
-                addTask(line);
-                }
+                System.out.println(LINE);
+                System.out.println("Unknown command!");
+                System.out.println(LINE);
             }
         }
+    }
 
-    private static void addTask(String Description){
-        if(counts >= 100){
+    private static void addTodo(String line){
+        if(TaskCounts >= MAX_TASKS){
             System.out.println(LINE);
             System.out.println("Sorry, I can only store up to 100 tasks!");
             System.out.println(LINE);
             return;
         }
 
-        tasks[counts] = new Task(Description);
-        counts++;
+        String description = line.substring(5).trim();
+        if(description.isEmpty()){
+            System.out.println(LINE);
+            System.out.println("The description of a Todo cannot be empty.");
+            System.out.println(LINE);
+            return;
+        }
+
+        tasks[TaskCounts] = new Todo(description);
+        TaskCounts++;
         System.out.println(LINE);
-        System.out.println("added: " + Description);
+        System.out.println("added Todo task: " + tasks[TaskCounts - 1]);
+        System.out.println("Now you have" + TaskCounts + "tasks in your list.");
         System.out.println(LINE);
     }
 
-    private static void showList(){
+    private static void addDeadline(String line){
+        if(TaskCounts >= MAX_TASKS){
+            System.out.println(LINE);
+            System.out.println("Sorry, I can only store up to 100 tasks!");
+            System.out.println(LINE);
+            return;
+        }
+
+        String remaining = line.substring(9).trim();
+        if(!remaining.contains("/by")){
+            System.out.println(LINE);
+            System.out.println("Please use the format: deadline <description> /by <date>");
+            System.out.println(LINE);
+            return;
+        }
+
+        String[] parts = remaining.split("/by", 2);
+        String description = parts[0].trim();
+        String by = parts[1].trim();
+
+        if (description.isEmpty() || by.isEmpty()){
+            System.out.println(LINE);
+            System.out.println("Description and deadline cannot be empty.");
+            System.out.println(LINE);
+            return;
+        }
+        tasks[TaskCounts] = new Deadline(description, by);
+        TaskCounts++;
+        System.out.println(LINE);
+        System.out.println("added Deadline task: " + tasks[TaskCounts - 1]);
+        System.out.println("Now you have" + TaskCounts + "tasks in your list.");
+        System.out.println(LINE);
+    }
+
+    private static void addEvent(String line){
+        if(TaskCounts >= MAX_TASKS){
+            System.out.println(LINE);
+            System.out.println("Sorry, I can only store up to 100 tasks!");
+            System.out.println(LINE);
+            return;
+        }
+
+        String remaining = line.substring(6).trim();
+        if(!remaining.contains("/from") || !remaining.contains("/to")){
+            System.out.println(LINE);
+            System.out.println("Please use the format: event <description> /from <start> /to <end>");
+            System.out.println(LINE);
+            return;
+        }
+
+        String[] parts = remaining.split("/from", 2);
+        String description = parts[0].trim();
+        String[] timeParts = parts[1].split("/to", 2);
+        if(timeParts.length < 2){
+            System.out.println(LINE);
+            System.out.println("Please use the format: event <description> /from <start> /to <end>");
+            System.out.println(LINE);
+            return;
+        }
+        String from = timeParts[0].trim();
+        String to = timeParts[1].trim();
+
+
+        if (description.isEmpty() || from.isEmpty() || to.isEmpty()){
+            System.out.println(LINE);
+            System.out.println("Description, start time and end time cannot be empty.");
+            System.out.println(LINE);
+            return;
+        }
+        tasks[TaskCounts] = new Event(description, from, to);
+        TaskCounts++;
+        System.out.println(LINE);
+        System.out.println("added Event task: " + tasks[TaskCounts - 1]);
+        System.out.println("Now you have " + TaskCounts + " tasks in your list.");
+        System.out.println(LINE);
+    }
+
+    private static void showTaskList(){
         System.out.println(LINE);
 
-        if(counts == 0){
+        if(TaskCounts == 0){
             System.out.println("Your task list is empty!");
         }else{
             System.out.println("Here are the tasks in your list:");
-            for(int i = 0; i < counts; i++){
+            for(int i = 0; i < TaskCounts; i++){
                 System.out.println((i + 1) + ". " + tasks[i]);
             }
         }
@@ -76,7 +171,7 @@ public class Yiyi {
         try {
             int taskNumber = Integer.parseInt(line.substring(5).trim());
 
-            if (taskNumber < 1 || taskNumber > counts) {
+            if (taskNumber < 1 || taskNumber > TaskCounts) {
                 System.out.println(LINE);
                 System.out.println("Task number " + taskNumber + " does not exist!");
                 System.out.println(LINE);
@@ -102,7 +197,7 @@ public class Yiyi {
         try{
             int taskNumber = Integer.parseInt(line.substring(7).trim());
 
-            if (taskNumber < 1 || taskNumber > counts) {
+            if (taskNumber < 1 || taskNumber > TaskCounts) {
                 System.out.println(LINE);
                 System.out.println("Task number " + taskNumber + " does not exist!");
                 System.out.println(LINE);
